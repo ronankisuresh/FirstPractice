bb787fd4a7405ebb9acbff00d130cd06eb9e52e9
ronankisuresh <ronanki.suresh@gmail.com>
2020-06-23 13:39:39 +0530

    new file added

---
+++ FirstMaven/src/test/java/Pack1/Sample2.java
@@ -0,0 +1,10 @@
+package test.java.pack1;
+
+public class Sample2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sample code2");
+	}
+
+}
