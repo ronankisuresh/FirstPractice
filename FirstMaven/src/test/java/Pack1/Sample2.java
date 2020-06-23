5e0fd18efba22dd9060a650ace07b5e09179d2c3
ronankisuresh <67310871+ronankisuresh@users.noreply.github.com>
2020-06-23 13:50:08 +0530

    changed directly

--- FirstMaven/src/test/java/Pack1/Sample2.java
+++ FirstMaven/src/test/java/Pack1/Sample2.java
@@ -5,6 +5,8 @@ public class Sample2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Sample code2");
+		System.out.println("Sample code3");
+		System.out.println("Sample code4");
 	}
 
 }
